7a602effef01ce4a958f6389410d977fdb55eeb0
gbrlevi <powerzinns@gmail.com>
2025-10-30 09:57:51 -0300

    first commit

---
+++ lib/toy_robot/robot.ex
@@ -0,0 +1,91 @@
+defmodule ToyRobot.Robot do
+  defstruct x: 0, y: 0, facing: :north
+
+  alias ToyRobot.Robot
+
+  @moduledoc """
+  Documentação para o ToyRobot.Robot
+  """
+
+  @doc """
+  Movimenta o robô uma posição para a direção que o mesmo está apontando.
+
+  ## exemplo
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %Robot{y: 0, facing: :north}
+    %Robot{y: 0, facing: :north}
+    iex> robot |> Robot.move
+    %Robot{y: 1}
+  """
+  def move(%Robot{facing: facing} = robot) do
+    case facing do
+      :north -> %{robot | y: robot.y + 1}
+      :south -> %{robot | y: robot.y - 1}
+      :east -> %{robot | x: robot.x + 1}
+      :west -> %{robot | x: robot.x - 1}
+    end
+  end
+
+  defp move_right(robot) do
+    %{robot | x: robot.x +1}
+  end
+
+  defp move_left(robot) do
+    %{robot | x: robot.x +1}
+  end
+
+  defp move_north(robot) do
+    %{robot | y: robot.y + 1}
+  end
+
+  defp move_south(robot) do
+    %{robot | y: robot.y -1}
+  end
+
+  @doc """
+  Gira o robô no sentido anti-horário (esquerda)
+
+  ## Exemplo
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %Robot{x: 0, y: 0, facing: :north}
+    %Robot{x: 0, y: 0, facing: :north}
+    iex> robot |> Robot.turn_left
+    %Robot{x: 0, y: 0, facing: :west}
+  """
+  def turn_left(%__MODULE__{facing: facing} = robot) do
+    new_facing =
+      case facing do
+        :north -> :west
+        :west -> :south
+        :south -> :east
+        :east -> :north
+      end
+
+    %{robot | facing: new_facing}
+  end
+
+  @doc """
+  Gira o robô no sentido horário (direita)
+
+  ## Exemplo
+    iex> alias ToyRobot.Robot
+    ToyRobot.Robot
+    iex> robot = %Robot{x: 0, y: 0, facing: :north}
+    %Robot{x: 0, y: 0, facing: :north}
+    iex> robot |> Robot.turn_right
+    %Robot{x: 0, y: 0, facing: :east}
+  """
+  def turn_right(%__MODULE__{facing: facing} = robot) do
+    new_facing =
+      case facing do
+        :north -> :east
+        :east -> :south
+        :south -> :west
+        :west -> :north
+      end
+
+    %{robot | facing: new_facing}
+  end
+end
